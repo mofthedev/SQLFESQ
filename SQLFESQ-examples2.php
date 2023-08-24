adc95a309c3db17cd868e7f2a07e92a5f2336d06
Muhammed Ömer Faruk Selvi <84385239+mofthedev@users.noreply.github.com>
2023-08-24 15:20:19 +0300

    Update SQLFESQ-examples2.php

--- SQLFESQ-examples2.php
+++ SQLFESQ-examples2.php
@@ -1,57 +1,59 @@
 <?php
 
-require "SQLFESQ.php";
+use MofSelvi\SQLFESQ\SQLFESQ;
 
-// $db = new SQLFESQ("localhost", "root", "", "appuidb");
+require dirname(__FILE__)."/SQLFESQ.php";
 
-// echo $db->errno.": ".$db->error."\n<br>\n";
+$db = new SQLFESQ("localhost", "root", "", "appuidb");
+
+echo $db->errno.": ".$db->error."\n<br>\n";
 
 
 
 
-// // $db->query("CREATE TABLE users(
-// //     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-// //     name varchar(200),
-// //     score int
-// //     );");
+$db->query("CREATE TABLE users(
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name varchar(200),
+    score int
+    );");
 
 
 
 
 // [C]RUD
-// // $newusers = [["muhammed", 0],["omer", 10],["faruk", 20], ["selvi", 30]];
-// // $db->query("INSERT INTO users (name,score) VALUES ", ...$newusers);
+$newusers = [["muhammed", 0],["omer", 10],["faruk", 20], ["selvi", 30]];
+$db->query("INSERT INTO users (name,score) VALUES ", ...$newusers);
 
-// // echo $db->last_query;
+echo $db->lastQuery;
 
 
 
 
 // C[R]UD
-// $db->fetch_type = MYSQLI_ASSOC;
-// $getusers = $db->query("SELECT * FROM users WHERE", ["score >="=>"12.2"]);
-// print_r($getusers);
+$db->fetchType = MYSQLI_ASSOC;
+$getusers = $db->query("SELECT * FROM users WHERE", ["score >="=>"12.2"]);
+print_r($getusers);
+print_r(array_keys($getusers[0]));
+echo json_encode($getusers);
+echo "\n<br>".$db->numOfRows." rows fetched.";
+echo "\n<br>\n----\n<br>\n";
+
+
+$db->fetchType = MYSQLI_NUM;
+$getusers = $db->query("SELECT * FROM users WHERE", ["score >="=>"12.2"]);
+print_r($getusers);
+print_r(array_keys($getusers[0]));
+echo "\n<br>".$db->numOfRows." rows fetched.";
+echo json_encode($getusers);
+
+
+$getusers = $db->query("SELECT * FROM users WHERE name LIKE", ["%mu%"]);
+$getusers = $db->query("SELECT * FROM users WHERE", ["OR" => [[ "name LIKE"=>"%mu%"], ["name LIKE"=>"%se%" ]] ]);
+print_r($getusers);
 // print_r(array_keys($getusers[0]));
-// echo json_encode($getusers);
-// echo "\n<br>".$db->num_of_rows." rows fetched.";
-// echo "\n<br>\n----\n<br>\n";
-
-
-// $db->fetch_type = MYSQLI_NUM;
-// $getusers = $db->query("SELECT * FROM users WHERE", ["score >="=>"12.2"]);
-// print_r($getusers);
-// print_r(array_keys($getusers[0]));
-// echo "\n<br>".$db->num_of_rows." rows fetched.";
-// echo json_encode($getusers);
-
-
-// $getusers = $db->query("SELECT * FROM users WHERE name LIKE", ["%mu%"]);
-// $getusers = $db->query("SELECT * FROM users WHERE", ["OR" => [[ "name LIKE"=>"%mu%"], ["name LIKE"=>"%se%" ]] ]);
-// print_r($getusers);
-// // print_r(array_keys($getusers[0]));
-// echo json_encode($getusers);
-// echo "\n<br>".$db->num_of_rows." rows fetched.";
-// echo "\n<br>\n----\n<br>\n";
+echo json_encode($getusers);
+echo "\n<br>".$db->numOfRows." rows fetched.";
+echo "\n<br>\n----\n<br>\n";
 
 
 
@@ -59,16 +61,16 @@ require "SQLFESQ.php";
 
 
 // CR[U]D
-// $db->query("UPDATE users SET", ["score =" => "15.5"],"WHERE",["score >" => "12.2"]);
-// $db->query("UPDATE users SET score=score+15","WHERE",["score >" => "12.2"]);
-// $db->query("UPDATE users SET score=score+",[15],"WHERE",["score >" => "12.2"]);
-// $db->query("UPDATE users SET name=CONCAT(name,",[" surname"],") WHERE",["score >" => "12.2"]);
-// echo $db->affected_rows." records have been updated.";
+$db->query("UPDATE users SET", ["score =" => "15.5"],"WHERE",["score >" => "12.2"]);
+$db->query("UPDATE users SET score=score+15","WHERE",["score >" => "12.2"]);
+$db->query("UPDATE users SET score=score+",[15],"WHERE",["score >" => "12.2"]);
+$db->query("UPDATE users SET name=CONCAT(name,",[" surname"],") WHERE",["score >" => "12.2"]);
+echo $db->affectedRows." records have been updated.";
 
 
 
 
-// C[R]UD
-// $db->query("DELETE FROM users WHERE", ["AND" => [["score >="=>"12.2"], ["score <="=>"22.2"]]]);
-// echo $db->affected_rows." records have been deleted.";
+// CRU[D]
+$db->query("DELETE FROM users WHERE", ["AND" => [["score >="=>"12.2"], ["score <="=>"22.2"]]]);
+echo $db->affectedRows." records have been deleted.";
 
